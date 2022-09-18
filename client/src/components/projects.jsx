d05a732ece3ff402613f15d0740f66110d1e43d8
jlangner87 <josh.langner@gmail.com>
2022-09-17 20:38:00 -0500

    styling project component

--- client/src/components/projects.jsx
+++ client/src/components/projects.jsx
@@ -1,4 +1,5 @@
 import recipes from '../assets/recipes.png'
+import mock from '../assets/mock.png'
 
 const Projects = () => {
   return (
@@ -7,14 +8,22 @@ const Projects = () => {
       <br></br>
       <div className="projects">
         <h1 className="title_text">Projects</h1>
-        <section className='carousel'></section>
+        <div className='carousel'>
         <div className="project_card">
           <h1 className='project_title'>Recipes For Me</h1>
           <img className='project_img' src={recipes}/>
-          <p className='content_text'> This is a description of the project and my role in the project</p>
+          <p className='project_text'> This is a description of the project and my role in the project</p>
           <a className='project_item' href='https://obscure-reef-40887.herokuapp.com/'>Deployed Site</a>
           <a className='project_item' href='https://github.com/jengriffin/Recipes-For-Me-'>Code Repo: Client</a>
           <a className='project_item' href='https://github.com/Aonmonomer/Recipe-For-Me-api'>Code Repo: API</a>
+        </div>
+        <div className="project_card">
+          <h1 className='project_title'>WikiBean</h1>
+          <img className='project_img' src={mock}/>
+          <p className='project_text'> This is a description of the project and my role in the project</p>
+          <a className='project_item' href='https://wikibean.netlify.app/beans'>Deployed Site</a>
+          <a className='project_item' href='https://github.com/jlangner87/roaster_roster_client'>Code Repo</a>
+        </div>
         </div>
       </div>
   </div>
