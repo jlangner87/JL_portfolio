afd805c17a4fa8087c1852bf4eb4cdd51546cc4b
jlangner87 <josh.langner@gmail.com>
2022-09-20 14:04:30 -0500

    clean up about me and projects

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -15,36 +15,37 @@ const About = () => {
       <br/>
       Drawing from my experiences, I effectively uncover root problems  with empathy and communicate abstract solutions in a concrete way that inspires confidence and understanding.
       </p>
-      <h1 className="about_title">Technically Speaking...</h1>
-      <p className="content_text">From the time I took apart a broken VCR and fixed it, so that I could watch my “Fellowship of the Ring” in my own room until now, I have always been fascinated by technology. I loved figuring what made the world tick&#8212;spending hours picking apart patterns and learning how to replicate them.</p>
-      <p className="content_text">Which leads to my first <em>big boy job</em>. In 2011, I was hired as a service technician by a cell phone company—fixing flip hinges, replacing screens, and soldering speakers in place. Over the years, I continually adapted to the rapidly changing industry. I went from fixing hardware to troubleshooting software, direct user support, and even teaching a smart device class for senior citizens in my community.</p>
-      <p className="content_text">The longer I worked there, the more my interests shifted to software. I keep asking myself,<br/> \
+      <h1 className="about_title">The Origin story...</h1>
+      <p className="content_text">From the time I took apart a broken VCR and fixed it, so that I could watch my “Fellowship of the Ring” in my own room until now, I have always loved figuring what made the world tick.</p>
+      <p className="content_text">Cue my first <em>big boy job</em> fixing flip hinges and replacing screens for a cellular company. Over the years, I continually adapted to the rapidly changing industry. I went from hardware repair to troubleshooting software, to direct user support, and even teaching a smart device class for senior citizens in my community.</p>
+      <p className="content_text">Over time, my interests shifted to software. I kept asking myself,<br/>
       <em>Where did this come from?</em> <br/>
       <em>If I developed this, how would I approach it?</em><br/>
       <em>What could I improve?</em></p>
-      <p className="content_text">In the spring of 2022, I took the leap and applied for the Software Engineering Immersive course at <em>General Assembly</em>. Now I am catapulting forward into new exciting aspects of developing every single day.</p>
+      <p className="content_text">I made the leap and took the Software Engineering Immersive course at <em>General Assembly</em>. Now I am catapulting forward into new exciting aspects of developing every single day.</p>
 
 
       <h1 className="about_title">On a personal note...</h1>
-      <p className="content_text">I am a husband to a beautiful wife and father of three wild kids. My family and I live in northwest Iowa, where I am heavily involved in the local community.</p>
-      <p className="content_text">Aside from tech, my other passion is theater. I am a playwright, director, and actor. It is fun, but I have gained so many transferrable skills in teater. I have learned how to effectively work with a diverse group to deliver the best possible product. I have learned the importance of giving my best to my areas of contribution. I have learned the process of taking an abstract concept and transforming it into a tangible product for the audience to enjoy.</p>
+      <p className="content_text">Aside from a developer, I am a husband and father living. My passion hobby is theater. I am a playwright, director, and actor. It is fun, but I have gained so many transferrable skills in teater. I have learned how to effectively work with a diverse group to deliver the best possible product. I have learned the importance of giving my best to my areas of contribution. I have learned the process of taking an abstract concept and transforming it into a tangible product for the audience to enjoy.</p>
 
-      <h1 className="about_title">Octopus Philosophy...</h1>
-      <p className="content_text"><em>What’s up with the octopus logo?</em><br/>
-        The octopus is an animal that represents the core pillars of my personal philosophy about engineering, work, and life in general.</p>
-      <p className="content_text"><b>Be Versatile</b><br/>
+      <h1 className="about_title">An Octopus Philosophy...</h1>
+      <p className="about_title2">Be Versatile</p>
+      <p className="content_text">
         The octopus can change its shape or color to match any environment—it is able to adjust to and handle even the most challenging situations <br/>
         <em>When faced with a challenge, keep your head up and know that you already have what it takes to adapt and overcome.</em>
       </p>
-      <p className="content_text"><b>Be Resourceful</b><br/>
+      <p className="about_title2">Be Resourceful</p>
+      <p className="content_text">
         The octopus is continually learning. They learn by encountering obstacles. They explore the obstacle, discover a solution, and then keep that knowledge to be built upon when facing future obstacles. <br/>
         <em>See each obstacle as an opportunity to learn. When you do not know how, use your past experiences and all available resources to learn how. </em></p>
-      <p className="content_text"><b>Be Attentive</b><br/>
+        <p className="about_title2">Be Attentive</p>
+      <p className="content_text">
       The octopus’s tentacles are an extension of its brain. They are hardwired with millions of neurons. It is continuously feeling the environment and updating its perspective. <br/>
       <em>Pay attention. Anticipate change. There are clues everywhere, telling us how to prevent or fix any problem. </em></p>
-      <p className="content_text"><b>Be Resilient</b><br/>
+      <p className="about_title2">Be Resilient</p>
+      <p className="content_text">
         The octopus faces many dangers, but it has an amazing survival mechanism. It can regenerate lost limbs. When attacked, the octopus does not give up and die. It heals and goes on living.<br/>
-        <em>When things get really hard&#8212;When you meet failure, do not quit. Get back up. Get better than you were before. Keep going.</em></p>
+        <em>When things get really hard&#8212;When you meet failure, do not quit. Get up. Keep going.</em></p>
       <div className="values">
         <p>Personal Values<br/>
           <em>Willingness to Learn</em> &nbsp; | &nbsp; <em>Continuous Improvement</em> &nbsp; | &nbsp; <em>Collaboration</em> &nbsp; | &nbsp; <em>Openness</em> &nbsp; | &nbsp; <em>Honesty</em> &nbsp; | &nbsp; <em>Integrity</em>
--- client/src/components/projects.jsx
+++ client/src/components/projects.jsx
@@ -13,6 +13,17 @@ const Projects = () => {
         <h1 className='scroll_instruction'>Scroll for more > > ></h1>
         <div className='carousel'>
         <div className="project_card">
+          <h1 className='project_title'>WikiBean</h1>
+          <img className='project_img' src={mock}/>
+          <p className='project_text'>WikiBean is a PERN stack application where I focus on building RESTful APIs and multiple query routes that can be accessed from my ReactJs front end that was styled with custom CSS.</p>
+          <p className='project_text'>The PSQL database with 4 models and 14 express routes delivering full crud operations to the user.</p>
+          <p className='project_text_footnote'>Be sure to check back and see where WikiBean is going. This is a passion project and will continue to evolve as I sharpen my existing skills and learn new ones.</p>
+          <p className='project_text_footnote'>I am currently looking for a new hosting service to replace Heroku for the backend of this project. Please view the demo below to see the full functionality of this application.</p>
+          <a className='project_item' href='https://wikibean.netlify.app/'>&#123;Demo&#125;</a><br/>
+          <a className='project_item' href='https://wikibean.netlify.app/'>&#123;Deployed Site&#125;</a><br/>
+          <a className='project_item' href='https://github.com/jlangner87/roaster_roster_client'>&#123;Code Repo&#125;</a><br/>
+        </div>
+        <div className="project_card">
           <h1 className='project_title'>Recipes For Me</h1>
           <img className='project_img' src={recipes}/>
           <p className='project_text'>Recipes For Me is a PERN stack application developed for a group project at General Assembly.</p>
@@ -20,16 +31,6 @@ const Projects = () => {
           <a className='project_item' href='https://obscure-reef-40887.herokuapp.com/'>&#123;Deployed Site&#125;</a><br/>
           <a className='project_item' href='https://github.com/jengriffin/Recipes-For-Me-'>&#123;Code Repo: React App&#125;</a><br/>
           <a className='project_item' href='https://github.com/Aonmonomer/Recipe-For-Me-api'>&#123;Code Repo: API&#125;</a>
-        </div>
-        <div className="project_card">
-          <h1 className='project_title'>WikiBean</h1>
-          <img className='project_img' src={mock}/>
-          <p className='project_text'>WikiBean is a PERN stack application where I focus on building RESTful APIs and multiple query routes that can be accessed from my ReactJs front end that was styled with custom CSS.</p>
-          <p className='project_text'>The PSQL database with 4 models and 14 express routes delivering full crud operations to the user.</p>
-          <p className='project_text_footnote'>Be sure to check back and see where WikiBean is going. This is a passion project and will continue to evolve as I sharpen my existing skills and learn new ones.</p>
-          <p className='project_text_footnote'>I am currently looking for a new hosting service to replace Heroku for the backend of this project.</p>
-          <a className='project_item' href='https://wikibean.netlify.app/'>&#123;Deployed Site&#125;</a><br/>
-          <a className='project_item' href='https://github.com/jlangner87/roaster_roster_client'>&#123;Code Repo&#125;</a><br/>
         </div>
         <div className="project_card">
           <h1 className='project_title_alice'>Rock Paper Scissors in Vue</h1>
@@ -41,7 +42,7 @@ const Projects = () => {
         <div className="project_card">
           <h1 className='project_title_alice'>Alice's Adventures in Python</h1>
           <img className='project_img' src={alice}/>
-          <p className='project_text'> This mini-project demonstrates control flow in Python in the form of a command line "choose your own adventure game."</p>
+          <p className='project_text'> This mini-project was an assignment from my time at General Assembly It demonstrates control flow in Python in the form of a command line "choose your own adventure game."</p>
           <p className='project_text_footnote'>There is no deployed site. This game is designed to be played in the Python CLI</p>
           <a className='project_item' href='https://github.com/jlangner87/control_flow_Py'>&#123;Code Repo&#125;</a>
         </div>
