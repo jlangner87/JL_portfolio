efb48e637b31e80a0156d39d4f9be719db3764a0
jlangner87 <josh.langner@gmail.com>
2022-09-26 14:27:15 -0500

    nav bar icons added

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,15 +1,21 @@
 import '../App.css'
+import info from '../assets/info.png'
+import skills from '../assets/skills.png'
+import briefcase from '../assets/briefcase.png'
+import care from '../assets/care.png'
+import contact from '../assets/contacts.png'
+import top from "../assets/up.png"
 
 const Nav = () => {
   return (
     <div className="nav">
       <div className="sidenav_bar">
-        <a href="#anchor_about">About</a>
-        <a href="#anchor_skills">Skills</a>
-        <a href="#anchor_projects">Projects</a>
-        <a href="#anchor_octopus">Values</a>
-        <a href="#anchor_contact">Contact</a>
-        <a href="#anchor_top">Top</a>
+        <a href="#anchor_about">&#160; <img className="nav_icon" src={info}/> &#160; About</a>
+        <a href="#anchor_skills">&#160;<img className="nav_icon" src={skills}/> &#160;Skills</a>
+        <a href="#anchor_projects">&#160;<img className="nav_icon" src={briefcase}/> &#160;Projects</a>
+        <a href="#anchor_octopus">&#160;<img className="nav_icon" src={care}/> &#160;Values</a>
+        <a href="#anchor_contact">&#160;<img className="nav_icon" src={contact}/> &#160;Contact</a>
+        <a href="#anchor_top">&#160;<img className="nav_icon" src={top}/> &#160;Top</a>
       </div>  
     </div>
   )
